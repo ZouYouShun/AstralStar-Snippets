587bf534650f3202211618a494eb3ceacb18e1c1
ZouYouShun <itisalanlife@gmail.com>
2020-06-25 21:52:52 +0800

    change hover to mouseenter

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  AfterViewInit,
+} from '@angular/core';
+
 import { ElectronService } from '../core';
 
 interface Snippet {
@@ -6,13 +15,26 @@ interface Snippet {
   value: string;
 }
 
+enum IpcEventType {
+  MESSAGE = 'MESSAGE',
+  HEIGHT = 'HEIGHT',
+  EXIT = 'EXIT',
+}
+
 @Component({
   selector: 'st-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent implements OnInit, AfterViewInit {
+  @ViewChild('main', { static: true }) mainElm: ElementRef<HTMLDivElement>;
+  @ViewChildren('item') items: QueryList<HTMLLIElement>;
+
   searchText = '';
+
+  currentHeight = 0;
+
+  selectIndex = 0;
 
   snippets: Snippet[] = [
     {
@@ -41,19 +63,72 @@ export class SearchBarComponent implements OnInit {
 
   constructor(private electronService: ElectronService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._setCurrentHeight();
+
+    window.addEventListener(
+      'keyup',
+      (e) => {
+        console.log('exit', e);
+        switch (e.code) {
+          case 'Escape':
+            this._sendIpc(IpcEventType.EXIT, true);
+            break;
+          case 'ArrowUp':
+            e.preventDefault();
+            this.selectIndex = Math.max(0, this.selectIndex - 1);
+            break;
+          case 'ArrowDown':
+            e.preventDefault();
+            this.selectIndex = Math.min(
+              this.searchResult.length - 1,
+              this.selectIndex + 1
+            );
+            break;
+          case 'Enter':
+            break;
+
+          default:
+            break;
+        }
+      },
+      true
+    );
+  }
+
+  private _setCurrentHeight(): void {
+    this.currentHeight = this.mainElm.nativeElement.clientHeight;
+    this._sendIpc(IpcEventType.HEIGHT, this.currentHeight);
+  }
+
+  ngAfterViewInit(): void {
+    this.items.changes.subscribe((x) => {
+      this._setCurrentHeight();
+    });
+  }
 
   change(e: any): void {
-    console.log(e);
     this.searchResult =
       e !== ''
         ? this.snippets.filter((snippet) =>
             `${snippet.value} ${snippet.key}`.includes(e)
           )
         : [];
+
+    this.currentHeight = this.mainElm.nativeElement.clientHeight;
+    this.selectIndex = 0;
   }
 
   choice(snippet: Snippet): void {
-    this.electronService.ipcRenderer.send('message', snippet);
+    this._sendIpc(IpcEventType.MESSAGE, snippet);
+  }
+
+  hover(i: number): void {
+    console.log(i);
+    this.selectIndex = i;
+  }
+
+  private _sendIpc(snippet: IpcEventType, value: any): void {
+    this.electronService.ipcRenderer.send(snippet, value);
   }
 }
