d02e19ab7490bb47ffa4441a6988f33db53e3487
ZouYouShun <itisalanlife@gmail.com>
2020-06-25 19:13:29 +0800

    tmp layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SearchDetailComponent } from './search-detail/search-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchBarComponent,
-    SearchDetailComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SearchBarComponent, SearchDetailComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Snippet {
+  key: string;
+  value: string;
+}
+
 @Component({
   selector: 'st-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
+  searchText = '';
 
-  constructor() { }
+  snippets: Snippet[] = [
+    {
+      key: 'ga',
+      value: 'ga',
+    },
+    {
+      key: 'ga1',
+      value: 'ga',
+    },
+    {
+      key: 'ga2',
+      value: 'ga',
+    },
+    {
+      key: 'ga3',
+      value: 'ga',
+    },
+    {
+      key: 'ga4',
+      value: 'ga',
+    },
+  ];
 
-  ngOnInit(): void {
+  searchResult = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  change(e: any): void {
+    console.log(e);
+    this.searchResult =
+      e !== ''
+        ? this.snippets.filter((snippet) =>
+            `${snippet.value} ${snippet.key}`.includes(e)
+          )
+        : [];
   }
 
+  choice(snippet: Snippet): void {
+    console.log(snippet);
+  }
 }
