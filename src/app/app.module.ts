848feb49b9b0be0a0d2474d74c5705a5b04a0c24
ZouYouShun <itisalanlife@gmail.com>
2020-06-25 17:31:36 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchDetailComponent } from './search-detail/search-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    SearchDetailComponent
   ],
   imports: [
     BrowserModule
