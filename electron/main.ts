b49613e8c96f56464f1b86826c6a2199a20c3c4b
ZouYouShun <itisalanlife@gmail.com>
2020-07-06 23:18:26 +0800

    combine two workspace together

--- electron/main.ts
+++ electron/main.ts
@@ -1,81 +1,109 @@
 import {
   app,
-  BrowserWindow,
-  screen,
-  globalShortcut,
-  Menu,
-  nativeTheme,
   clipboard,
+  BrowserWindow,
+  BrowserWindowConstructorOptions,
+  globalShortcut,
+  ipcMain,
+  Menu,
+  screen,
 } from 'electron';
 import * as path from 'path';
-import * as url from 'url';
 import * as robot from 'robotjs';
+import * as url from 'url';
+
+interface Snippet {
+  key: string;
+  value: string;
+}
+
+const enum IpcEventType {
+  COPY = 'COPY',
+  APPLY = 'APPLY',
+  APPLY_COPY = 'APPLY_COPY',
+  HEIGHT = 'HEIGHT',
+  EXIT = 'EXIT',
+  PREVIOUS_SNIPPET = 'PREVIOUS_SNIPPET',
+  GO_SETTINGS = 'GO_SETTINGS',
+  INIT_ROUTE = 'INIT_ROUTE',
+}
+
+const DEFAULT_HEIGHT = 60;
 
 let win: BrowserWindow = null;
 const args = process.argv.slice(1),
   serve = args.some((val) => val === '--serve');
 
-function createWindow(): BrowserWindow {
+const rootUrl = process.cwd();
+
+let previousText = '';
+let currentRoute = '';
+
+function createWindow(
+  route = '',
+  option: BrowserWindowConstructorOptions & { blurClose?: boolean } = {
+    blurClose: true,
+  }
+): BrowserWindow {
   const electronScreen = screen;
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
+  currentRoute = route;
 
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: size.width / 4,
-    y: size.height * 0.05,
-    width: size.width / 2,
-    height: 600,
-
-    webPreferences: {
-      nodeIntegration: true,
-      allowRunningInsecureContent: serve ? true : false,
-    },
-    alwaysOnTop: true,
-    frame: false,
-    transparent: true,
-    // closable: false,
-    // resizable: false,
-  });
-
-  win.hide();
-
-  // Get mouse cursor absolute position
-  const { x, y } = screen.getCursorScreenPoint();
-  // Find the display where the mouse cursor will be
-  const currentDisplay = screen.getDisplayNearestPoint({ x, y });
-  // Set window position to that display coordinates
-  win.setPosition(currentDisplay.workArea.x, currentDisplay.workArea.y);
-  // Center window relatively to that display
-  win.center();
-  // Display the window
-  win.show();
-
-  if (serve) {
-    // win.webContents.openDevTools();
-
-    require('electron-reload')(__dirname, {
-      electron: require(`${__dirname}/node_modules/electron`),
-    });
-    win.loadURL('http://localhost:4200');
+  if (win) {
+    win.setSize(size.width / 2, DEFAULT_HEIGHT);
+    win.show();
   } else {
-    win.loadURL(
-      url.format({
-        pathname: path.join(__dirname, 'dist/index.html'),
-        protocol: 'file:',
-        slashes: true,
-      })
-    );
-  }
+    console.log('init window');
+    win = new BrowserWindow({
+      width: size.width / 2,
+      height: DEFAULT_HEIGHT,
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    console.log('close');
-    // process.exit();
-    win = null;
-  });
+      webPreferences: {
+        nodeIntegration: true,
+        allowRunningInsecureContent: serve ? true : false,
+      },
+      alwaysOnTop: true,
+      frame: false,
+      transparent: true,
+      resizable: false,
+      ...option,
+    });
+
+    if (option.blurClose) {
+      win.on('blur', () => {
+        hideWindow();
+      });
+    }
+
+    win.once('closed', () => {
+      win = null;
+    });
+
+    // if (serve) {
+    //   // win.webContents.openDevTools();
+    //   require('electron-reload')(rootUrl, {
+    //     electron: require(`${rootUrl}/node_modules/electron`),
+    //   });
+    // }
+  }
+  win.loadURL(path.join('http://localhost:4200'));
+
+  // if (serve) {
+  //   win.loadURL(path.join('http://localhost:4200'));
+  // } else {
+  //   win.loadURL(
+  //     url.format({
+  //       pathname: path.join(__dirname, 'dist/index.html'),
+  //       protocol: 'file:',
+  //       slashes: true,
+  //     })
+  //   );
+  // }
+
+  const { x, y } = screen.getCursorScreenPoint();
+  const currentDisplay = screen.getDisplayNearestPoint({ x, y });
+  win.setPosition(currentDisplay.workArea.x, currentDisplay.workArea.y);
+  win.center();
 
   return win;
 }
@@ -83,48 +111,87 @@ function createWindow(): BrowserWindow {
 try {
   app.allowRendererProcessReuse = true;
 
-  // nativeTheme.on('updated', function theThemeHasChanged() {
-  //   console.log(nativeTheme.shouldUseDarkColors);
-  // });
-
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
   app.whenReady().then(() => {
     const dockMenu = Menu.buildFromTemplate([
       {
-        label: 'New Window',
+        label: 'Settings',
         click() {
-          console.log('New Window');
+          openSetting();
         },
       },
-      {
-        label: 'New Window with Settings',
-        submenu: [{ label: 'Basic' }, { label: 'Pro' }],
-      },
-      { label: 'New Commands...' },
     ]);
-
     app.dock.setMenu(dockMenu);
 
     globalShortcut.register('CommandOrControl+shift+X', () => {
-      // const originText = clipboard.readText();
-
-      // clipboard.writeText('Example String');
-
-      // console.log(clipboard.readText());
-
-      // robot.keyTap('v', process.platform === 'darwin' ? 'command' : 'control');
-
-      // clipboard.writeText(originText);
-      if (win) {
-        win.close();
-        // win = null;
-      }
-
       createWindow();
     });
+    globalShortcut.register('CommandOrControl+shift+V', () => {
+      setTimeout(() => {
+        robot.keyTap(
+          'v',
+          process.platform === 'darwin' ? 'command' : 'control'
+        );
+      }, 200);
+    });
+
+    ipcMain
+      .on(IpcEventType.APPLY, (event, arg: Snippet) => {
+        previousText = arg.value;
+
+        hideWindow();
+
+        setTimeout(() => {
+          robot.typeString(arg.value);
+        }, 50);
+
+        event.returnValue = 'success';
+      })
+      .on(IpcEventType.COPY, (event, arg: Snippet) => {
+        previousText = arg.value;
+        clipboard.writeText(arg.value);
+        hideWindow();
+
+        event.returnValue = 'success';
+      })
+      .on(IpcEventType.APPLY_COPY, (event, arg: Snippet) => {
+        previousText = arg.value;
+        clipboard.writeText(arg.value);
+        hideWindow();
+
+        setTimeout(() => {
+          robot.typeString(arg.value);
+        }, 50);
+
+        event.returnValue = 'success';
+      })
+      .on(IpcEventType.PREVIOUS_SNIPPET, (event) => {
+        event.returnValue = previousText;
+      })
+      .on(IpcEventType.GO_SETTINGS, (event) => {
+        if (win) {
+          win.close();
+          win = null;
+        }
+        setTimeout(() => {
+          openSetting();
+        }, 500);
+        event.returnValue = 'success';
+      })
+      .on(IpcEventType.INIT_ROUTE, (event) => {
+        event.returnValue = currentRoute;
+        currentRoute = '';
+      })
+      .on(IpcEventType.HEIGHT, (event, value: number) => {
+        const size = win.getSize();
+        win.setSize(size[0], DEFAULT_HEIGHT + value);
+
+        event.returnValue = 'success';
+      })
+      .on(IpcEventType.EXIT, (event) => {
+        hideWindow();
+
+        event.returnValue = 'success';
+      });
   });
 
   // Quit when all windows are closed.
@@ -146,4 +213,24 @@ try {
 } catch (e) {
   // Catch Error
   // throw e;
+}
+
+function openSetting() {
+  if (win) {
+    win.close();
+    win = null;
+  }
+  createWindow('settings', {
+    height: 600,
+    frame: true,
+    alwaysOnTop: false,
+    transparent: false,
+    resizable: true,
+    blurClose: false,
+  });
+}
+
+// for return focus with previous tab
+function hideWindow() {
+  Menu.sendActionToFirstResponder('hide:');
 }
