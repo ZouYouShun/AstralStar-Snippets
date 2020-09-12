64f942cd3e7f3e67f0da8ea07b8f23f0ec772af1
ZouYouShun <itisalanlife@gmail.com>
2020-09-12 14:58:24 +0800

    fix debug way

--- electron/main.dev.ts
+++ electron/main.dev.ts
@@ -1,12 +1,14 @@
+import * as path from 'path';
 import { MainElectronApp } from './MainElectronApp';
 
-const rootUrl = process.cwd();
+// const rootUrl = path.join(process.cwd(), '/dist/electron');
 
-require('electron-reload')(rootUrl, {
-  electron: require(`${rootUrl}/node_modules/electron`),
-  hardResetMethod: 'exit',
-  forceHardReset: true,
-});
+// require('electron-reload')(rootUrl, {
+//   electron: require(`${process.cwd()}/node_modules/electron`),
+//   argv: ['--remote-debugging-port=9222'],
+//   hardResetMethod: 'exit',
+//   forceHardReset: true,
+// });
 
 new MainElectronApp({
   loadUrl: 'http://localhost:4200',
